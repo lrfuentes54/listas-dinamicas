afb6353f6853a24f9156632c01b09abbf48fd44a
lrfuentes54 <lfuentes@ucab.edu.ve>
2024-04-16 03:15:26 -0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int resp;
+	do {
+		cout << "\n\n\tMENU\n\n";
+		cout << "\t\t1) Imprimir lista\n";
+		cout << "\t\t2) Insertar elemento\n";
+		cout << "\t\t3) Eliminar elemento\n";
+		cout << "\t\t0) Salir\n\n";
+		cout << "\tMarque su opcion: ";
+		cin >> resp;
+        switch (resp) {
+            case 1:
+                /* code */
+                break;
+
+            case 2:
+                /* code */
+                break;
+
+            case 3:
+                /* code */
+                break;    
+            
+            default:
+                break;
+        }
+		
+    } while(resp);
+}
